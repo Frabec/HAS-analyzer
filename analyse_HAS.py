bc45d584d9513d767c8fa8cfe2257f6866e13f7e
Franco Rabec <franco.rabec@hotmail.fr>
2019-06-03 18:28:17 -0700

    Added wavelength nothing is debugged, have to check with labview program if values are correct...

--- analyse_HAS.py
+++ analyse_HAS.py
@@ -10,7 +10,7 @@ def browse_button():
     folder_path.set(filename)
 
 #get in a folder open all the files and stores them in an array, using a double array format
-def form_folder_to_arrays(path):
+def form_folder_to_arrays(path, wavelength):
     data_storage= []
     data_storage_counter=0
     for file_names in os.listdir(path):
@@ -22,7 +22,7 @@ def form_folder_to_arrays(path):
         for lines in f:
             raw=(lines.split('\t'))
             #replace NaN by 0 and convert to float
-            filtered=list(map(lambda x: float(x) if not("NaN" in x) else 0., raw))
+            filtered=list(map(lambda x: wavelength*float(x) if not("NaN" in x) else 0., raw))
             #if list is not empty
             data_storage[data_storage_counter].append(filtered)
         data_storage_counter=data_storage_counter+1
@@ -42,8 +42,9 @@ def calculate_RMS_pixel_by_pixel(data):
         
 
 
-def calculate_everything(path_to_folder):
-    data=form_folder_to_arrays(path_to_folder)
+def calculate_everything(path_to_folder, wavelength_str):
+    wavelength=int(wavelength_str)
+    data=form_folder_to_arrays(path_to_folder, wavelength)
     RMS_images=calculate_RMS_images(data)
     global mean_RMS
     mean_RMS.set(np.mean(RMS_images))
@@ -55,32 +56,36 @@ def calculate_everything(path_to_folder):
 
 root = Tk()
 root.title("HAS Analyzer")
-root.geometry("500x500")
+root.geometry("600x300")
 folder_path = StringVar()
 mean_RMS= DoubleVar()
 RMS_RMS = DoubleVar()
 RMS_pixels =DoubleVar()
+Wavelength = DoubleVar()
+Wavelength.set(800.0)
 lbl1 = Label(master=root,textvariable=folder_path)
 lbl1.grid(row=0, column=2)
 button2 = Button(text="Browse folder", command=browse_button)
 button2.grid(row=0, column=1)
-button3 = Button(text="Do it", command=lambda: calculate_everything(folder_path.get()))
+button3 = Button(text="Do it", command=lambda: calculate_everything(folder_path.get(), Wavelength.get()))
 button3.grid(row=0, column=0)
 #titles
-lbl_mean_rms_global_title = Label(master=root, text="Mean of the RMS of the images: ")
-lbl_mean_rms_global_title.grid(row=1, column=0, sticky="W")
-lbl_rms_rms_global_title = Label(master=root, text="RMS of the RMS of the images: ")
-lbl_rms_rms_global_title.grid(row=2, column=0, sticky="W")
-lbl_rms_pixel_by_pixel_title = Label(master=root, text="Mean of RMS pixel by pixel: ")
-lbl_rms_pixel_by_pixel_title.grid(row=3, column=0, sticky="W")
-
-
+lbl_mean_rms_global_title = Label(master=root, text="Mean of the RMS of the images(nm): ")
+lbl_mean_rms_global_title.grid(row=2, column=0, sticky="W",pady=(50,10))
+lbl_rms_rms_global_title = Label(master=root, text="RMS of the RMS of the images(nm): ")
+lbl_rms_rms_global_title.grid(row=3, column=0, sticky="W",pady=10)
+lbl_rms_pixel_by_pixel_title = Label(master=root, text="Mean of RMS pixel by pixel(nm): ")
+lbl_rms_pixel_by_pixel_title.grid(row=4, column=0, sticky="W",pady=10)
+lbl_wavelength = Label(master=root, text="Wavelength(nm): ")
+lbl_wavelength.grid(row=1, column=0, sticky="E", pady=(20,0))
+entry_wavelength = Entry(master=root, textvariable=Wavelength)
+entry_wavelength.grid(row=1, column=1, sticky="W", pady=(20,0))
 #values
 lbl_mean_rms_global_value = Label(master=root, textvariable=mean_RMS)
-lbl_mean_rms_global_value.grid(row=1, column=1, sticky="W")
+lbl_mean_rms_global_value.grid(row=2, column=1, sticky="W",pady=(50,10))
 lbl_rms_rms_global_value = Label(master=root, textvariable=RMS_RMS)
-lbl_rms_rms_global_value.grid(row=2, column=1, sticky="W")
+lbl_rms_rms_global_value.grid(row=3, column=1, sticky="W",pady=10)
 lbl_rms_pixel_by_pixel_value = Label(master=root, textvariable=RMS_pixels)
-lbl_rms_pixel_by_pixel_value.grid(row=3, column=1, sticky="W")
+lbl_rms_pixel_by_pixel_value.grid(row=4, column=1, sticky="W",pady=10)
 
 mainloop()
