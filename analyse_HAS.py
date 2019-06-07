f56df94848f41067b1ee6f0d5c5292ab8aa17f96
Franco Rabec <franco.rabec@hotmail.fr>
2019-06-07 10:01:36 -0700

    Wavefront has nothing to do with wavelength !!

--- analyse_HAS.py
+++ analyse_HAS.py
@@ -20,7 +20,7 @@ def browse_button():
         button3.config(state="disabled")
         button4.config(state="disabled")
 #get in a folder open all the files and stores them in an array, using a double array format
-def form_folder_to_arrays(path, wavelength):
+def form_folder_to_arrays(path):
     data_storage= []
     for file_names in os.listdir(path):
         #the files containing wavefront information are in wft extension
@@ -31,7 +31,8 @@ def form_folder_to_arrays(path, wavelength):
         for lines in f:
             raw=(lines.split('\t'))
             #replace NaN by 0 and convert to float
-            modified=list(map(lambda x: wavelength*float(x) if not("NaN" in x) else 0., raw))
+            #*1000 to have values in nm
+            modified=list(map(lambda x: 1000*float(x) if not("NaN" in x) else 0., raw))
             #if list is not empty
             new_image.append(modified)
         #Now data_storage contains a list of images of same xlength and ylength, but we have converted the NaN to zeros
@@ -71,11 +72,10 @@ def calculate_RMS_pixel_by_pixel(data):
         RMS_map.append(row_for_map)
     return RMS_pixels
 
-def calculate_everything(path_to_folder, wavelength_str):
+def calculate_everything(path_to_folder):
     #Reset RMS map
     global RMS_map
-    wavelength=int(wavelength_str)
-    data=form_folder_to_arrays(path_to_folder, wavelength)
+    data=form_folder_to_arrays(path_to_folder)
     RMS_images=calculate_RMS_images(data)
     global mean_RMS
     mean_RMS.set(np.mean(RMS_images))
@@ -91,7 +91,7 @@ def calculate_everything(path_to_folder, wavelength_str):
 def plotRMS():
     global RMS_map
     fig, ax = plt.subplots()
-    im= ax.imshow(RMS_map, interpolation='bilinear', cmap=cm.YlOrRd, origin='lower', extent=[-2,2,-2,2])
+    im= ax.imshow(RMS_map, interpolation='hermite', cmap=cm.YlOrRd, origin='lower', extent=[-2,2,-2,2])
     cbar= fig.colorbar(im, label="RMS in nm")
     ###increase label size of colorbar
     ax = cbar.ax
@@ -105,7 +105,7 @@ def plotRMS():
 
 root = Tk()
 root.title("HAS Analyzer")
-root.geometry("600x300")
+root.geometry("800x300")
 #######################################
 ###           Global vars           ###
 #######################################
@@ -113,8 +113,6 @@ folder_path = StringVar()
 mean_RMS= DoubleVar()
 RMS_RMS = DoubleVar()
 RMS_pixels =DoubleVar()
-Wavelength = DoubleVar()
-Wavelength.set(800.0)
 #store RMS map
 RMS_map=[]
 ########################################
@@ -128,7 +126,7 @@ lbl1 = Label(master=root,textvariable=folder_path)
 lbl1.grid(row=0, column=3)
 button2 = Button(text="Browse folder", command=browse_button)
 button2.grid(row=0, column=2)
-button3 = Button(text="Do it", state="disabled",command=lambda: calculate_everything(folder_path.get(), Wavelength.get()))
+button3 = Button(text="Do it", state="disabled",command=lambda: calculate_everything(folder_path.get()))
 button3.grid(row=0, column=0)
 button4 = Button(text="Show map of wavefront", state="disabled", command=plotRMS)
 button4.grid(row=0, column=1)
@@ -139,10 +137,6 @@ lbl_rms_rms_global_title = Label(master=root, text="RMS of the RMS of the images
 lbl_rms_rms_global_title.grid(row=3, column=0, sticky="W",pady=10)
 lbl_rms_pixel_by_pixel_title = Label(master=root, text="Mean of RMS pixel by pixel(nm): ")
 lbl_rms_pixel_by_pixel_title.grid(row=4, column=0, sticky="W",pady=10)
-lbl_wavelength = Label(master=root, text="Wavelength(nm): ")
-lbl_wavelength.grid(row=1, column=0, sticky="E", pady=(20,0))
-entry_wavelength = Entry(master=root, textvariable=Wavelength)
-entry_wavelength.grid(row=1, column=1, sticky="W", pady=(20,0))
 #values
 lbl_mean_rms_global_value = Label(master=root, textvariable=mean_RMS)
 lbl_mean_rms_global_value.grid(row=2, column=1, sticky="W",pady=(50,10))
